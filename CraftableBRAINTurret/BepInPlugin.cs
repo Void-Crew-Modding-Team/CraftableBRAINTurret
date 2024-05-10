99ac71b19dabe1b4c5f80252fa6c47f5f83c3008
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-05-10 16:33:47 -0700

    Add project files.

---
+++ CraftableBRAINTurret/BepInPlugin.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using CG.Client.UserData;
+using CG.Ship.Object;
+using HarmonyLib;
+using ResourceAssets;
+using System.Reflection;
+
+namespace CraftableBRAINTurret
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Void Crew.exe")]
+    [BepInDependency("VoidManager")]
+    public class BepinPlugin : BaseUnityPlugin
+    {
+        static FieldInfo UnlockOptionsFI = AccessTools.Field(typeof(UnlockItemDef), "unlockOptions");
+
+        private void Awake()
+        {
+            GUIDUnion GUID = new GUIDUnion("6b2c6be56b6678643bd0039305890622");
+
+            //Setup Crafting rules (alloy or insription, cost and recyclability)
+            CraftingRules cr = new()
+            {
+                CraftingMethod = CraftMethod.ItemExchange,
+                Recycle = new ResourceCurrency(ResourceType.Alloys, 4),
+                ItemExchangeInfo = new CraftableWithItemExchange(ItemExchangeType.BlankBox, 1),
+                CanBeRecycled = true
+            };
+
+
+            //Sets crafting recipe
+            ResourceAssetContainer<CraftingDataContainer, UnityEngine.Object, CraftableItemDef>.Instance.GetAssetDefById(GUID).crafting = cr;
+
+            //Unlocks for crafting
+            UnlockOptionsFI.SetValue(ResourceAssetContainer<UnlockContainer, UnityEngine.Object, UnlockItemDef>.Instance.GetAssetDefById(GUID), new UnlockOptions() { UnlockCriteria = UnlockCriteriaType.Always });
+
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
---
+++ CraftableBRAINTurret/VoidManagerPlugin.cs
@@ -0,0 +1,13 @@
+﻿using VoidManager.MPModChecks;
+
+namespace CraftableBRAINTurret
+{
+    public class VoidManagerPlugin : VoidManager.VoidPlugin
+    {
+        public override MultiplayerType MPType => MultiplayerType.All;
+
+        public override string Author => "Dragon";
+
+        public override string Description => "Makes B.R.A.I.N. turrets craftable. Requires All clients to install.";
+    }
+}
